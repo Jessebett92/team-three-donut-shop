63ba7a0f0c23bc6230a2191b64de4d54f4d1e8b8
jessebettridge <jessebett92@gmail.com>
2019-07-31 13:29:07 -0600

    created git ignore w/ pages directory

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Auth from "./pages/auth";
+
 export default class App extends Component {
   render() {
     return (
