ea88a05991da4f8df43bf482de9bb60780126860
Raisa Bustos <raisa.bustosv@gmail.com>
2019-07-31 14:03:18 -0600

    creating database

--- app.py
+++ app.py
@@ -17,3 +17,34 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 #We need to decide names of our tables/classes
+class Product(db.Model):
+    __tablename__ = "products"
+    product_id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(50))
+    product_price = db.Column(db.Float(500))
+
+    def __init__(self, product_name, product_price):
+        self.product_name = product_name
+        self.price = price
+
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "product_name", "product_price")
+
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
+
+@app.route("/") #root page
+def greeting():
+    return "<h1>Donut shop API in construction...</h1>" #displayed hello in the homepage or root
+
+@app.route("/products",methods=["GET"])
+def get_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result.data)
+
+
+if __name__ =="__main__":
+    app.debug = True
+    app.run()    
