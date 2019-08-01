ca2bd896a54f38693da90c87b7caf8a617dd0764
jessebettridge <jessebett92@gmail.com>
2019-08-01 11:01:29 -0600

    added auth component"

--- src/components/app.js
+++ src/components/app.js
@@ -2,13 +2,61 @@ import React, { Component } from "react";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Home from "./pages/home";
 import Auth from "./pages/auth";
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN"
+    };
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+    this.handleUnSuccessfulLogin = this.handleUnSuccessfulLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    });
+  }
+
+  handleUnSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+
+  handleLogout() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+
   render() {
     return (
       <div className="app">
-        <h1>Donut Shop</h1>
+        <h1>{this.state.loggedInStatus}</h1>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+
+            <Route
+              path="/auth"
+              render={props => (
+                <Auth
+                  {...props}
+                  handleSuccessfulLogin={this.handleSuccessfulLogin}
+                  handleUnSuccessfulLogin={this.handleUnSuccessfulLogin}
+                  handleLogout={this.handleLogout}
+                  loggedInStatus={this.state.loggedInStatus}
+                />
+              )}
+            />
+          </Switch>
+        </Router>
       </div>
     );
   }
--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,11 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Auth = () => {
-  return (
-    <div>
-      <h1>Auth Component</h1>
-    </div>
-  );
-};
+class Auth extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = () => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleLogin = event => {
+    event.preventDefault();
+    if (this.state.username === "donuts" && this.state.password === "login") {
+      this.props.handleSuccessfulLogin();
+      this.props.history.push("/");
+    } else {
+      this.props.handleUnSuccessfulLogin();
+    }
+  };
+
+  render() {
+    return (
+      <div className="auth-page-wrapper">
+        <div className="auth-welcome">
+          <h1>Login to Your Account</h1>
+        </div>
+        <div className="login-component-wrapper">
+          <div className="auth-left">
+            <div className="auth-image">
+              <img src="https://cdn-image.foodandwine.com/sites/default/files/1519939593/ohio-donut-trail-2-FT-BLOG0318.jpg" />
+            </div>
+          </div>
+          <div className="auth-right">
+            <form className="auth-input" onSubmit={this.handleLogin}>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                placeholder="Enter Username"
+                name="username"
+                value={this.state.username}
+              />
+              <input
+                type="password"
+                placeholder="Enter Password"
+                onChange={this.handleChange}
+                name="password"
+                value={this.state.password}
+              />
+              <button className="btn" type="submit">
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Auth;
